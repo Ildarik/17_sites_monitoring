0e6f426c8b9c36ab810aeae42767d36ef0c35cd6
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-29 23:32:54 +0300

    Define load urls & Define respond_ok

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,26 @@
-def load_urls4check(path):
-    pass
+import requests
+from whois import whois
+from sys import argv
+from datetime import datetime
+
+# TODO freeze > requirements
+
+def load_urls4check(filepath):
+    with open(filepath, "r") as textfile:
+        urls = textfile.readlines()
+    return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    return requests.get(url).ok
+
 
 def get_domain_expiration_date(domain_name):
     pass
 
+
 if __name__ == '__main__':
-    pass
+    filepath = argv[1]
+    urls = load_urls4check(filepath)
+    for url in urls:
+        pass
