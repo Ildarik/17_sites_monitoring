7a4c070e0c541801fd866f68fff2ec445fd5e23e
Nogmanov Ildar <nogmanov@gmail.com>
2017-07-30 10:47:19 +0300

    Define get expiration date

--- check_sites_health.py
+++ check_sites_health.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 def load_urls4check(filepath):
     with open(filepath, "r") as textfile:
-        urls = textfile.readlines()
+        urls = textfile.read().splitlines()
     return urls
 
 
@@ -16,11 +16,16 @@ def is_server_respond_with_200(url):
 
 
 def get_domain_expiration_date(domain_name):
-    pass
+    whois_data = whois(domain_name)
+    return whois_data['expiration_date']
 
 
 if __name__ == '__main__':
     filepath = argv[1]
     urls = load_urls4check(filepath)
     for url in urls:
-        pass
+        print(url)
+        print('Отвечает на запрос статусом HTTP 200: ', is_server_respond_with_200(url))
+        tdelta = get_domain_expiration_date(url) - datetime.now()
+        print('Доменное имя проплачено как минимум на 1 месяц вперед: ', tdelta.days > 30)
+        print()
